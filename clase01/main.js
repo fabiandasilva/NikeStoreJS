54427a5721d93441356daa54382b24e9b9132fb9
fabiandasilva <fabian.da.silva@hotmail.com>
2021-12-04 11:58:45 -0300

    Finalice el primer desafio

---
+++ clase01/main.js
@@ -0,0 +1,13 @@
+//Operaciones matematicas
+alert("Comencemos con una suma");
+let numeroUno = Number(prompt("Ingrese el primer numero"));
+let numeroDos = Number(prompt("Ingrese el segundo numero"));
+let resultado = numeroUno + numeroDos + " fue el resultado 😵 ";
+alert(resultado);
+
+//Ingresar datos
+let nombre = prompt("Ahora ingresa tu nombre ");
+let apellido = prompt("y tu apellido ");
+let salida =
+  "Tu nombre y apellido es " + "  " + nombre + " " + apellido + "  🌝 ";
+alert(salida);
