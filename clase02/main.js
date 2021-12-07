15efeacba45082831a7e4dd50991499e1811ce69
fabiandasilva <fabian.da.silva@hotmail.com>
2021-12-07 17:48:43 -0300

    Correcciones finalizadas

--- clase02/main.js
+++ clase02/main.js
@@ -1,28 +1,14 @@
-let perdirNumero = prompt("Escribe un numero del 1 al 1000");
-if (perdirNumero > 0 && perdirNumero <= 100) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-} else if (perdirNumero > 101 && perdirNumero <= 200) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-} else if (perdirNumero > 201 && perdirNumero <= 300) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-} else if (perdirNumero > 301 && perdirNumero <= 400) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-} else if (perdirNumero > 401 && perdirNumero <= 500) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-} else if (perdirNumero > 501 && perdirNumero <= 600) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-}
- else if (perdirNumero > 601 && perdirNumero <= 700) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-}
- else if (perdirNumero > 701 && perdirNumero <= 800) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-}
- else if (perdirNumero > 801 && perdirNumero <= 900) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-}
- else if (perdirNumero > 901 && perdirNumero <= 1000) {
-  console.log("Tu numero esta dentro del rango 😄😄");
-}else if(perdirNumero >= 1001){
-    console.log("Uff te pasaste, proba de nuevo 🤔🤔🤔")
+let pedirNumero = Number(prompt("Escribe un numero del 1 al 1000"));
+
+if (pedirNumero > 0 && pedirNumero <= 1000) {
+  alert("Tu numero esta dentro del rango 😄😄😄");
+
+} 
+else if (pedirNumero >= 1001) {
+  alert("Uff te pasaste, proba de nuevo 🤔🤔🤔");
+
+} 
+else if ((pedirNumero != Number)) {
+  alert("Ingresaste una letra proba de nuevo 🙅‍♂️🙅‍♂️🙅‍♂️");
+
 }
