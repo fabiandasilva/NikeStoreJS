34fd13aaa6083bc1bd166a7a880ca10c43ad2cb7
fabiandasilva <fabian.da.silva@hotmail.com>
2021-12-10 13:25:56 -0300

    Agregue else

--- clase02/main.js
+++ clase02/main.js
@@ -1,14 +1,18 @@
 let pedirNumero = Number(prompt("Escribe un numero del 1 al 1000"));
 
-if (pedirNumero > 0 && pedirNumero <= 1000) {
-  alert("Tu numero esta dentro del rango 😄😄😄");
+if (pedirNumero >= 1 && pedirNumero <= 1000) {
+	alert("Tu numero esta dentro del rango 😄😄😄");
+}
 
-} 
-else if (pedirNumero >= 1001) {
-  alert("Uff te pasaste, proba de nuevo 🤔🤔🤔");
+else if(pedirNumero < 1){
+	alert("Uff ingresaste un numero menor a 1, proba de nuevo 🤔🤔🤔");	
 
-} 
-else { ((pedirNumero != Number)) 
-  alert("Ingresaste una letra proba de nuevo 🙅‍♂️🙅‍♂️🙅‍♂️");
+}
+else if(pedirNumero > 1000){
+	alert("Uff te pasaste, proba de nuevo 🤔🤔🤔");
+
+}
+else {
+	alert("Ingresaste algo que no es un numero 🙅‍♂️🙅‍♂️🙅‍♂️");
 
 }
