8f617c3720c19f530d392b5de214dd76dfed1b34
fabiandasilva <fabian.da.silva@hotmail.com>
2021-12-13 00:44:05 -0300

    Realizando cambios en el desafio 03

---
+++ clase03/entrega/main.js
@@ -0,0 +1,10 @@
+
+
+let pedirTexto = prompt("Ingresa una palabra ");
+
+while (pedirTexto != "esc") {
+    
+  alert("El usuario ingreso " + pedirTexto);
+
+  pedirTexto = prompt("Ingresa otra palabra");
+}
