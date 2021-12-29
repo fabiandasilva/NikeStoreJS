ad2229da8d933af1fb95b63a55524c5f77e93c95
fabiandasilva <fabian.da.silva@hotmail.com>
2021-12-29 17:16:28 -0300

    Proyecto final Primera entrega

---
+++ proyectJS/js/main.js
@@ -0,0 +1,99 @@
+
+
+alert("Registrate antes de finalizar la compra");
+
+class newUser{
+    constructor(nombre, apellido, telefono, correo){
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.telefono = telefono;
+      this.correo = correo;
+    }
+   
+    mostrarDatos(){
+      console.log(`Los datos cargados fueron: \n${this.nombre}\n${this.apellido}\n${this.telefono}\n${this.correo}`)
+      alert("Registro exitoso!")
+    }
+  }
+  const nombre = prompt('Nombre: ')
+  const apellido = prompt('Apellido: ')
+  const telefono = prompt('Telefono: ')
+  const correo = prompt('Correo: ')
+  
+  const user = new newUser(nombre, apellido, telefono, correo)
+  
+  console.log(user)
+  
+  user.mostrarDatos();
+  
+   
+
+ 
+  
+  class Producto {
+    constructor(categoria, modelo, talle, precio) {
+ 
+      this.categoria = categoria;
+      this.modelo = modelo;
+      this.talle = talle;
+      this.precio = precio;
+    }
+  
+ 
+    sumarIva(){
+      this.precio=this.precio*1.21;
+    }
+  }
+ 
+  let arrayProductos =[];
+  
+  arrayProductos.push(new Producto("Correr","Nike Air Zoom Pegasus 38",42,18000));
+  
+  arrayProductos.push(new Producto("Correr","Epic React Flyknit 2",42,18000));
+  
+  arrayProductos.push(new Producto("Correr","Nike Joyride Dual Run",42,24728));
+  
+  arrayProductos.push(new Producto("Correr","Nike Renew Run 2",42,14728));
+  
+  arrayProductos.push(new Producto("Correr","Nike Revolution 5",42,30000));
+  
+  arrayProductos.push(new Producto("Entrenar","Nike Air Max Bella Training 4",42,30000));
+  
+  arrayProductos.push(new Producto("Entrenar","Nike City Trainer",42,32000));
+  
+  arrayProductos.push(new Producto("Entrenar","Nike In-Season Tr-9",42,42000));
+  
+  arrayProductos.push(new Producto("Entrenar","Nike legend Essential 2",42,42000));
+  
+  arrayProductos.push(new Producto("Entrenar","Nike Superrep Go",42,52000));
+  
+  arrayProductos.push(new Producto("Urbano","Nike Jordan air max 200",42,35000));
+  
+  arrayProductos.push(new Producto("Urbano","Nike Jordan air max 90",42,35000));
+  
+  arrayProductos.push(new Producto("Urbano","Nike Court Legacy",42,33000));
+  
+  arrayProductos.push(new Producto("Urbano","Nike Crater impact",42,35000));
+  
+  arrayProductos.push(new Producto("Urbano","Nike Wearallday",42,32000));
+  
+
+
+
+  for (let index = 0; index < arrayProductos.length; index++) {
+
+    arrayProductos[index].sumarIva();
+  }
+
+let indexProductoSeleccionado = 2;
+
+let cost = 0;
+
+let cantidad = parseInt(prompt("Su producto elegido fue:"+"\n"+"Categoria: "+arrayProductos[indexProductoSeleccionado].categoria+"\n"+"Modelo: "+arrayProductos[indexProductoSeleccionado].modelo+"\n"+"Talle: "+arrayProductos[indexProductoSeleccionado].talle+"\nPrecio: $"+arrayProductos[indexProductoSeleccionado].precio+"\nIngrese cantidad que desea comprar"));
+
+cost = cost + (cantidad*arrayProductos[indexProductoSeleccionado].precio);
+
+alert("El costo total fue de: "+cost) ;
+
+
+
