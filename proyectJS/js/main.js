4e414511113d64aeb811f873cfca1733520625f5
fabiandasilva <fabian.da.silva@hotmail.com>
2022-01-23 01:54:03 -0300

    Aca funciona todo

--- proyectJS/js/main.js
+++ proyectJS/js/main.js
@@ -1,134 +1,128 @@
-//!Llamo los elementos que voy a utilizar
-const contenedorProductos = document.getElementById('card-group');
-const contenedorCarrito = document.getElementById('contenedor-carrito');
-const contenedorProductosCarrito = document.getElementById('contenedor-productos-carrito');
-let nombreCapturado = document.getElementById("nombre").value;
-let correoCapturado = document.getElementById("correo").value;
+$(() => {
+    renderProducts(productos, contenedorProductos);
+    renderCart(getStorage(), contenedorCarrito);
+})
 
+const contenedorProductos = document.getElementById("containerProducts");
+const contenedorCarrito = $("#containerCart");
 
-//!Creo arrays para ir cargandolo
-const productos = [];
-let carrito = [];
+function renderProducts(productos, etiqueta) {
+    etiqueta.innerHTML = "";
+    for (producto of productos) {
+        etiqueta.innerHTML += `        
+        <div class="col-md-6 col-lg-4 col-xl-3 p-2">
+        <div class="special-img position-relative overflow-hidden">
+            <img src="${producto.imagen}" class="w-100"></a>
+        </div>
+        <div class="text-center">
+            <p class="text-capitalize mt-3 mb-1">${producto.modelo}</p>
+            <span class="fw-bold d-block">${producto.precio}</span>
+            <button class="agregar btn btn-primary mt-3" id="btnAdd${producto.id}">Agregar</button>
+            <button class="favorito btn btn-primary mt-3" id="btnFav${producto.id}"><i class="fa fa-heart"></i></button>
+        </div>
+    </div>
+       `
+    };
+    capturaBotonAgregar();
+};
 
+function renderCart(cart, container) {
+    let totalCompra = 0;
+    let totalUnidades = 0;
+    container.html("");
+    for (const product of cart) {
+        totalCompra += product.precio * product.cantidad;
+        container.prepend(`
+           <tr class="">
+               <td><span id="btnMinus${product.id}" class="btn">-</span>      ${product.cantidad}     <span id="btnPlus${product.id}" class="btn">+</span></td>
+               <td>${product.modelo}</td>
+               <td>${product.precio}</td>
+               <td>${product.precio*product.cantidad}</td>                
+               <td><button id="btnDel${product.id}" class="quitar btn btn-danger">X</button></td>
+           </tr>`);
 
-const producto1 = new Producto(1, "./img/running/Epic_React__Flyknit_2/imagen1.jpg", "Correr", "Nike Air Zoom Pegasus 38", 18000);
-const producto2 = new Producto(2, "./img/running/Nike_ Air_ Zoom_Pegasus_ 38/imagen1.jpg", "Correr", "Nike Epic React Flyknit 2", 18000);
-const producto3 = new Producto(3, "./img/running/Nike_Joyride_Dual_Run/imagen1.jpg", "Correr", "Nike Renew Run 2", 14728);
-const producto4 = new Producto(4, "./img/running/Nike_Renew_Run_2/imagen1.jpg", "Correr", "Nike Joyride Dual Run", 24728);
-const producto5 = new Producto(5, "./img/running/Nike_Revolution_5/imagen1.jpg", "Correr", "Nike Revolution 5", 30000);
-
-const producto6 = new Producto(6, "./img/training/Nike_air_Max_Bella_trainning_4/imagen1.jpg", "Entrenar", "Nike Air Max Bella Training 4", 30000);
-const producto7 = new Producto(7, "./img/training/Nike_city_trainer/imagen1.jpg", "Entrenar", "Nike City Trainer", 32000);
-const producto8 = new Producto(8, "./img/training/Nike_In-Season-Tr_9/imagen1.jpg", "Entrenar", "Nike In-Season Tr-9", 42000);
-const producto9 = new Producto(9, "./img/training/Nike_legend_Essentia_2/imagen1.jpg", "Entrenar", "Nike legend Essential 2", 42000);
-const producto10 = new Producto(10, "./img/training/Nike_Superrep_Go/imagen1.jpg", "Entrenar", "Nike Superrep Go", 52000);
-
-const producto11 = new Producto(11, "./img/urban/Jordan_air_max_200/imagen1.jpg", "Urbano", "Nike Jordan air max 200", 35000);
-const producto12 = new Producto(12, "./img/urban/Nike_Air_Max_90/imagen1.jpg", "Urbano", "Nike Jordan air max 90", 35000);
-const producto13 = new Producto(13, "./img/urban/Nike_Court_Legacy/imagen1.jpg", "Urbano", "Nike Court Legacy", 33000);
-const producto14 = new Producto(14, "./img/urban/Nike_Crater_impact/imagen1.jpg", "Urbano", "Nike Crater impact", 35000);
-const producto15 = new Producto(15, "./img/urban/Nike_Wearallday/imagen1.jpg", "Urbano", "Nike Wearallday", 42000);
-
-productos.push(producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8, producto9, producto10, producto11, producto12, producto13, producto14, producto15);
-
-
-//!Utilizo la funcion para ver lo que hay en el Storage
-if (ObtenerStorage('carrito')) {
-    carrito = ObtenerStorage('carrito');
-    renderizarCarrito(contenedorProductosCarrito, carrito);
-} 
-
-
-//!Renderizo los Productos
-renderizarProductos(contenedorProductos, productos);
-
-
-//!Creo una funcion para ir agregando al carrito
-function agregarAlCarrito(event) {
-    /* Mensaje de producto agregado al carrito */
-    Swal.fire({
-        icon: 'success',
-        title: 'Producto Agregado',
-        timer: 700
-    })
-    console.log(event.target.id);
-    const idProductoElegido = (event.target.id);
-
-    //!Matcheo con el id
-    if (productos.some(producto => producto.id === parseInt(idProductoElegido))) {
-
-        if (carrito.some(producto => producto.id === parseInt(idProductoElegido))) {
-            const productoElegido = carrito.find(producto => producto.id === parseInt(idProductoElegido));
-            productoElegido.cantidad++;
-            GuardarStorage('carrito', carrito);
-        } else {
-            const productoElegido = productos.find(producto => producto.id === parseInt(idProductoElegido));
-            const producto = new Carrito(productoElegido.id, productoElegido.modelo, productoElegido.precio, 1);
-            carrito.push(producto);
-            GuardarStorage('carrito', carrito);
-        }
-        //!Renderizo los productos para mostrarlos en el carrito
-        carrito = ObtenerStorage('carrito');
-        renderizarCarrito(contenedorProductosCarrito, carrito);
-    } else {
-        alert('Elija un producto valido');
+        $("#btnDel" + product.id).on("click", (e) => {
+            quitar(product.id);
+        });
+        $("#btnPlus" + product.id).on("click", () => {
+            product.cantidad++
+            setStorage(cart)
+            renderCart(cart, container)
+        })
+        $("#btnMinus" + product.id).on("click", () => {
+            product.cantidad--
+            setStorage(cart)
+            renderCart(cart, container)
+        })
     }
+    cart.map(e => totalUnidades += e.cantidad)
+    container.append(`
+       <tr>    
+           <th scope="row">${totalUnidades}</th>
+           <td colspan="2" class="">Totales</td>
+           <td>${totalCompra}</td>
+       </tr>`);
 }
 
-
-//!Renderizo los productos seleccionados al carrito
-function renderizarCarrito(contenedor, array) {
-    let precioTotal = 0;
-    //!Limpio todo 
-    contenedor.innerHTML = "";
-    array.forEach(producto => {
-        contenedor.innerHTML += `                 
-            <div class="row shoppingCartItem" id="contenedor-productos-carrito">
-        <div class="col-6">
-            <div
-                class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <img class="shopping-cart-image">
-                <h6
-                    class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">
-                   ${producto.modelo}
-                </h6>
-            </div>
-        </div>
-        <div class="col-2">
-            <div
-                class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${producto.precio}</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="ml-4 mb-0 shoppingCartCount">${producto.cantidad}</p>
-                <button class="btn btn-danger" id="buttonDelete" type="button">X</button>
-            </div>
-        </div>
-    </div>
-    `;
-    });
-
-
-    //!Realizo la suma y cantidad para determinar el precio
-    array.forEach(producto => precioTotal += producto.precio * producto.cantidad);
-    contenedor.innerHTML += `<div class="row">
-    <div class="col-12">
-        <div class="shopping-cart-total d-flex align-items-center">
-            <p class="mb-0">Total:  $${precioTotal} </p>
-            <div class="toast ml-auto bg-info" role="alert" aria-live="assertive"
-                aria-atomic="true" data-delay="2000">
-            </div>
-            <button class="btn btn-success ml-auto comprarButton" type="button"
-                data-toggle="modal" data-target="#comprarModal"id="finalizarCompra">Finalizar compra</button>
-        </div>
-    </div>
-</div>`
+function setStorage(array) {
+    localStorage.setItem("carrito", JSON.stringify(array));
 }
 
+function getStorage() {
+    return JSON.parse(localStorage.getItem("carrito")) || [];
+}
+
+function capturaBotonAgregar() {
+    const botones = document.getElementsByClassName("agregar");
+    for (const boton of botones) {
+        boton.addEventListener("click", (e) => {
+            agregar((e.target.id).substring(6))
+        });
+    };
+};
 
 
 
+function agregar(id) { //cuando invocas la funcion dentro del evento le pasas el id que te llego
+    const arrayCarrito = getStorage(); // asignacion condicional, se asigna el localStorage si existe o sino un array vacio
+    const prodSelec = productos.find(e => e.id === id); //busco el objeto cuya propiedad coincida con el id capturado por el evento y le agrego un propiedad cantidad
+    const prodCart = {
+        id: prodSelec.id,
+        nombre: prodSelec.nombre,
+        precio: prodSelec.precio,
+        cantidad: 1
+    };
+    let index = arrayCarrito.findIndex(e => e.id === id); //busco, si existe, el indice del producto seleccionado
+    index == -1 ? arrayCarrito.push(prodCart) : arrayCarrito[index].cantidad++; //agrego el objeto que encontre en el paso antetior
+    setStorage(arrayCarrito); // guardo el array nuevo dentro del storage
+    renderCart(arrayCarrito, contenedorCarrito) // muestro el array carrito, en este punto podria crear un nuevo render tipo tabla, por ej
+};
 
+function quitar(id) {
+    const arrayFinal = JSON.parse(localStorage.getItem("carrito")).filter(e => e.id != id);
+    setStorage(arrayFinal);
+    renderCart(arrayFinal, contenedorCarrito);
+}
+
+$("#filtroTodos").click((e) => {
+    e.preventDefault();
+    const filterTodos = productos.filter(e => e.categoria == "Training","Running","Urban");
+    renderProducts(filterTodos, contenedorProductos);
+});
+$("#filtroTraining").click((e) => {
+    e.preventDefault();
+    const filterTraining = productos.filter(e => e.categoria == "Training");
+    renderProducts(filterTraining, contenedorProductos);
+});
+
+$("#filtroRunning").click((e) => {
+    e.preventDefault()
+    const filterRunning = productos.filter(e => e.categoria == "Running");
+    renderProducts(filterRunning, contenedorProductos);
+});
+
+$("#filtroUrban").click((e) => {
+    e.preventDefault()
+    const filterUrban = productos.filter(e => e.categoria == "Urban");
+    renderProducts(filterUrban, contenedorProductos);
+});
---
+++ proyectJS/js/products.js
@@ -0,0 +1,110 @@
+//!Defino los productos en un array
+const productos = [{
+        id: "1",
+        modelo: "Nike Air Zoom Pegasus 38",
+        precio: 18000,
+        categoria: "Correr",
+        imagen: "./img/running/Epic_React__Flyknit_2/imagen1.jpg"
+    },
+    {
+        id: "2",
+        modelo: "Nike Epic React Flyknit 2",
+        precio: 18000,
+        categoria: "Correr",
+        imagen: "./img/running/Nike_ Air_ Zoom_Pegasus_ 38/imagen1.jpg"
+    },
+    {
+        id: "3",
+        modelo: "Nike Renew Run 2",
+        precio: 14728,
+        categoria: "Correr",
+        imagen: "./img/running/Nike_Joyride_Dual_Run/imagen1.jpg"
+    },
+    {
+        id: "4",
+        modelo: "Nike Joyride Dual Run",
+        precio: 24728,
+        categoria: "Correr",
+        imagen: "./img/running/Nike_Renew_Run_2/imagen1.jpg"
+    },
+    {
+        id: "5",
+        modelo: "Nike Revolution 5",
+        precio: 30000,
+        categoria: "Correr",
+        imagen: "./img/running/Nike_Revolution_5/imagen1.jpg",
+    },
+
+    {
+        id: "6",
+        modelo: "Nike Air Max Bella Training 4",
+        precio: 30000,
+        categoria: "Entrenar",
+        imagen: "./img/training/Nike_air_Max_Bella_trainning_4/imagen1.jpg",
+    },
+    {
+        id: "7",
+        modelo: "Nike City Trainer",
+        precio: 32000,
+        categoria: "Entrenar",
+        imagen: "./img/training/Nike_city_trainer/imagen1.jpg",
+    },
+    {
+        id: "8",
+        modelo: "Nike In-Season Tr-9",
+        precio: 42000,
+        categoria: "Entrenar",
+        imagen: "./img/training/Nike_In-Season-Tr_9/imagen1.jpg",
+    },
+    {
+        id: "9",
+        modelo: "Nike legend Essential 2",
+        precio: 42000,
+        categoria: "Entrenar",
+        imagen: "./img/training/Nike_legend_Essentia_2/imagen1.jpg",
+    },
+    {
+        id: "10",
+        modelo: "Nike Superrep Go",
+        precio: 52000,
+        categoria: "Entrenar",
+        imagen: "./img/training/Nike_Superrep_Go/imagen1.jpg",
+    },
+
+    {
+        id: "11",
+        modelo: "Nike Jordan air max 200",
+        precio: 35000,
+        categoria: "Urbano",
+        imagen: "./img/urban/Jordan_air_max_200/imagen1.jpg",
+    },
+    {
+        id: "12",
+        modelo: "Nike Jordan air max 90",
+        precio: 35000,
+        categoria: "Urbano",
+        imagen: "./img/urban/Nike_Air_Max_90/imagen1.jpg",
+    },
+    {
+        id: "13",
+        modelo: "Nike Court Legacy",
+        precio: 33000,
+        categoria: "Urbano",
+        imagen: "./img/urban/Nike_Court_Legacy/imagen1.jpg",
+    },
+    {
+        id: "14",
+        modelo: "Nike Crater impact",
+        categoria: "Urbano",
+        precio: 35000,
+        imagen: "./img/urban/Nike_Crater_impact/imagen1.jpg",
+    },
+    {
+        id: "15",
+        modelo: "Nike Wearallday",
+        precio: 42000,
+        categoria: "Urbano",
+        imagen: "./img/urban/Nike_Wearallday/imagen1.jpg",
+    },
+
+];
